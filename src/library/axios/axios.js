6f2b5532dea012cb75f46e977a7ede3729d64c68
nguyenphuctuan0907 <anhjack1102@gmail.com>
2022-11-08 16:45:00 +0700

    fix domain

--- src/library/axios/axios.js
+++ src/library/axios/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import { AXIOS_CONFIGS } from "../../constants/api";
 
-const baseURLdev = "https://newdev.befitter.io/befadmin";
+const baseURLdev = "https://newdev.befitter.io/admin";
 const baseURLProduction = "https://api.befitter.io/admin/";
 const isDebug = true;
 const baseURL = isDebug ? baseURLdev : baseURLProduction;
